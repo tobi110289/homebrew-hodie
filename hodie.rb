f2f0e772f3248c97a67fbdb6a8eb0198d8e38f55
tobi110289 <tobias.feltes@web.de>
2025-02-18 18:20:18 +0100

    fix(hodie.rb): fix release archive url

--- hodie.rb
+++ hodie.rb
@@ -3,7 +3,7 @@ require "formula"
 class Hodie < Formula
   desc "A productivity CLI suite with Pomodoro timer"
   homepage "https://github.com/tobi11089/hodie_cli"
-  url "https://github.com/tobi11089/hodie_cli/v1.0.0.tar.gz"
+  url "https://github.com/tobi11089/hodie_cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
